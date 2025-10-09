3dd38597f8028eb977ab3a1f9d5579b413869a61
Yami <yamilaveralopez@gmail.com>
2025-10-09 20:00:04 -0300

    add models schemas prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { ChatGateway } from './modules/chat/chat.gateway';
 
 @Module({
   imports: [UserModule],
+  providers: [ChatGateway],
 })
 export class AppModule {}
