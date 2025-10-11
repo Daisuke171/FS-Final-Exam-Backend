0a84dd8eff7f92c9745788f2bd18160a95c65ad9
Nicron <nicron007@gmail.com>
2025-10-11 07:19:31 +0000

    refactor(src/games): moved the games content to web-sockets folder

--- src/games/rock-paper-scissors/rps.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RpsGateway } from './gateway/rps.gateway';
-import { RpsService } from './rps.service';
-
-@Module({
-  providers: [RpsGateway, RpsService],
-})
-export class RpsModule {}
