a94230117f92a2573f4dff35303b2bd9eb834e86
Agustin <deadmanwonderland171@gmail.com>
2025-10-11 03:59:24 -0300

    moved games folder to modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { CodingWarModule } from './games/coding-war/coding-war.module';
-import { RpsModule } from './games/rock-paper-scissors/rps.module';
+import { CodingWarModule } from './modules/games/coding-war/coding-war.module';
+import { RpsModule } from './modules/games/rock-paper-scissors/rps.module';
 import { UserModule } from './modules/user/user.module';
 import { ChatGateway } from './modules/chat/chat.gateway';
 
