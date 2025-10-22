90623a3f4ab5d4332d16de0f3f9781aba41f98c3
Nicron <nicron007@gmail.com>
2025-10-22 03:46:59 +0000

    feat(rps.module): imported the authmodule

--- src/modules/games/web-sockets/rock-paper-scissors/rps.module.ts
+++ src/modules/games/web-sockets/rock-paper-scissors/rps.module.ts
@@ -3,9 +3,10 @@ import { RpsGateway } from './gateway/rps.gateway';
 import { RpsService } from './rps.service';
 import { GamesModule } from '@modules/games/games.module';
 import { UserModule } from '@modules/user/user.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   providers: [RpsGateway, RpsService],
-  imports: [GamesModule, UserModule],
+  imports: [GamesModule, UserModule, AuthModule],
 })
 export class RpsModule {}
